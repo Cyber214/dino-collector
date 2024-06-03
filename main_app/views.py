d49e9dcac95974cbf38183d1f4bbd9dd6cadc90d
Seun Dada <seun.dada777@gmail.com>
2024-06-03 12:24:02 -0400

    using templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,7 @@
+# We're already importing render from django.shortcuts
 from django.shortcuts import render
 
-# Add the following import
-from django.http import HttpResponse
+...
 
-# Define the home view
-def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+def about(request):
+  return render(request, 'about.html')
