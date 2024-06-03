0cfb1af56e60ba1b13d2ef7f8940c416fbb19609
Seun Dada <seun.dada777@gmail.com>
2024-06-03 19:07:36 -0400

    add real database model connected to the neon web

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,9 @@
 # We're already importing render from django.shortcuts
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Dino
 
-class Dino:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
 
-dinos = [
-  Dino('Troy', 'Tyrannosaurus', 'The king of dinosaurs.', 8),
-  Dino('Tina', 'Triceratops', 'Known for its three horns.', 5),
-  Dino('Vicky', 'Velociraptor', 'Swift and intelligent predator.', 3),
-  Dino('Stella', 'Stegosaurus', 'Characterized by its bony plates and tail spikes.', 10)
-]
-
+# Views
 def home(request):
   return render(request, 'home.html')
 
@@ -23,4 +11,5 @@ def about(request):
   return render(request, 'about.html')
 
 def dino_index(request):
-  return render(request, 'dinos/index.html', { 'dinos': dinos })
+    dinos = Dino.objects.all()
+    return render(request, 'dinos/index.html', { 'dinos': dinos })
