97d04b9d5fb87e2e162a6ab465936633f7908a83
Seun Dada <seun.dada777@gmail.com>
2024-06-04 09:58:16 -0400

    add login logout and signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('dinos/', views.dino_index, name='dino-index'),
   path('dinos/<int:dino_id>/', views.dino_detail, name='dino-detail'),
   path('dinos/create/', views.DinoCreate.as_view(), name='dino-create'),
   path('dinos/<int:pk>/update/', views.DinoUpdate.as_view(), name='dino-update'),
   path('dinos/<int:pk>/delete/', views.DinoDelete.as_view(), name='dino-delete'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,16 @@
 # We're already importing render from django.shortcuts
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 from .models import Dino
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Views
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
@@ -21,8 +25,15 @@ def dino_detail(request, dino_id):
 
 class DinoCreate(CreateView):
   model = Dino
-  fields = ['name', 'breed', 'description', 'age']
-  success_url = '/dinos/'
+  fields = ['name', 'species', 'description', 'age']
+  
+  # This inherited method is called when a
+  # valid dino form is being submitted
+  def form_valid(self, form):
+    # Assign the logged in user (self.request.user)
+    form.instance.user = self.request.user  # form.instance is the dino
+    # Let the CreateView do its job as usual
+    return super().form_valid(form)
 
 class DinoUpdate(UpdateView):
   model = Dino
@@ -31,3 +42,23 @@ class DinoUpdate(UpdateView):
 class DinoDelete(DeleteView):
   model = Dino
   success_url = '/dinos/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in
+      login(request, user)
+      return redirect('cat-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
+  # Same as: return render(request, 'signup.html', {'form': form, 'error_message': error_message})
